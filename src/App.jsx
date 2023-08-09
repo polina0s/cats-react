e60ca4e9c49e6c87dcac765d43f349cf35fdfde2
polina0s <polina.osintseva000@gmail.com>
2023-08-09 19:19:52 +0400

    change project structure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import './main.scss';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,9 +20,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
