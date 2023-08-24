f86976bf58b1c32f8c104a48b34e8015a7524102
polina0s <polina.osintseva000@gmail.com>
2023-08-24 16:09:41 +0400

    fixed layout for errorPage

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -1,16 +1,22 @@
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
 import { useRouteError } from 'react-router-dom';
+
+import errorPage from './errorPage.module.scss';
 
 export function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Container className={errorPage.cont}>
+      <h1 className="mb-4">Sorry, an unexpected error has occurred</h1>
+      <div className="mb-4">
+        <img src="../../public/pics/logo.png"></img>
+      </div>
+      <Button variant="info" href="/">
+        Go to home page
+      </Button>
+    </Container>
   );
 }
